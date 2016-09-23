ec92a5c733bdaf0203e8735ef04665f04e967035
Mario Jerkovic <mariojerkovic8@gmail.com>
2016-09-23 16:13:40 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mjerkovic on 23/09/2016.
+ */
---
+++ webpack.dll.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		vendor: [
+			'react',
+			'react-dom',
+			'material-ui',
+			'react-relay',
+			'react-router',
+			'react-router-relay',
+			'react-relay-network-layer',
+			'error-stack-parser',
+			'redbox-react',
+			'react-transform-catch-errors',
+			'react-transform-hmr',
+			'babel-runtime/core-js',
+		]
+	},
+	output: {
+		filename: '[name].dll.js',
+		path: path.join(__dirname, 'vendor_dll'),
+		name: '[name]_[hash]',
+		library: '[name]_[hash]'
+	},
+	module: {
+		exprContextRegExp: /$^/,
+		exprContextCritical: false,
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		}, {
+			test: /\.scss$/,
+			loaders: ['style-loader', 'css-loader', 'postcss-loader']
+		}, {
+			test: /\.json$/,
+			loader: 'json-loader'
+		}, {
+			test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+			loader: 'url-loader?limit=10000&name=assets/[hash].[ext]'
+		}]
+	},
+	plugins: [
+		new webpack.DllPlugin({
+			name: '[name]_[hash]',
+			path: path.join(__dirname, 'vendor_dll/[name]-manifest.json')
+		})
+	]
+};
